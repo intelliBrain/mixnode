8adfc6701e4b811e682c19132478280e6618b638
Ivan Mrvelj <sk8raid@gmail.com>
2016-05-28 23:33:52 +0200

    Sample test setup

---
+++ karma.conf.js
@@ -0,0 +1,36 @@
+var webpack = require('webpack');
+
+module.exports = function (config) {
+  config.set({
+
+    browsers: [ 'Chrome' ],
+
+    singleRun: true,
+
+    frameworks: [ 'mocha' ],
+
+    files: [
+      './test/tests.webpack.js'
+    ],
+
+    preprocessors: {
+      './test/tests.webpack.js': [ 'webpack', 'sourcemap' ]
+    },
+
+    reporters: [ 'dots' ],
+
+    webpack: {
+      devtool: 'inline-source-map',
+      module: {
+        loaders: [
+          { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ }
+        ]
+      }
+    },
+
+    webpackServer: {
+      noInfo: true
+    }
+
+  });
+};
---
+++ test/sample-test.js
@@ -0,0 +1,8 @@
+import chai from 'chai';
+var expect = chai.expect;
+
+describe('sample', function () {
+  it('should', function () {
+      expect(1 === 1);
+  });
+});
