e2a79e04119bcacef325db183d4b704f31ea225d
Ivan Mrvelj <sk8raid@gmail.com>
2016-12-21 23:01:28 +0100

    progress: WORKING LOGIN + /me

--- src/user/user-navigation.component.js
+++ src/user/user-navigation.component.js
@@ -1,12 +1,15 @@
 const {ipcRenderer} = require('electron');
 import React, {Component} from 'react';
+import Axios from 'axios';
 
 class UserNav extends Component {
     constructor () {
         super();
         this.logIn = this.logIn.bind(this);
         ipcRenderer.on('user-log-in', (event, args) => {
+            const token = args;
             localStorage.setItem('token', args);
+            Axios.get('https://api.mixcloud.com/me/?access_token=' + token).then(res => console.log(res));
         });
     }
 
