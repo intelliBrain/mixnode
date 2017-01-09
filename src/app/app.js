c08881e4d553ce4822b1f4ffb4ab0d82443e2e3e
Ivan Mrvelj <sk8raid@gmail.com>
2017-01-09 04:38:45 +0100

    chore: renaming

--- src/app/app.js
+++ src/app/app.js
@@ -13,7 +13,7 @@ class AppContainer extends Component {
     constructor(props) {
         super(props);
         this.registerAuthListener = this.registerAuthListener.bind(this);
-        this.mixcloudLogIn = this.mixcloudLogIn.bind(this);
+        this.bootstrapUser = this.bootstrapUser.bind(this);
     }
 
     componentDidMount () {
@@ -21,11 +21,11 @@ class AppContainer extends Component {
         this.registerAuthListener();
         if(localStorage.getItem('token')) {
             const token = localStorage.getItem('token');
-            this.mixcloudLogIn(token);
+            this.bootstrapUser(token);
         }
     }
 
-    mixcloudLogIn(token) {
+    bootstrapUser(token) {
         const { dispatch } = this.props;
         Axios.get('https://api.mixcloud.com/me/?access_token=' + token).then(
             (res) => dispatch(logIn(res.data, token)),
