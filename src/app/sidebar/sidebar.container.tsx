c0c3144ba7e91a7a5923dec8f42eb4b47398dab6
Ivan Mrvelj <sk8raid@gmail.com>
2017-05-07 18:09:05 +0200

    Hiding items from sidebar if not logged in: init

--- src/app/sidebar/sidebar.container.tsx
+++ src/app/sidebar/sidebar.container.tsx
@@ -1,25 +1,35 @@
+import { Dispatch } from 'redux';
 import { connect } from 'react-redux';
 import * as React from 'react';
 import { push } from 'react-router-redux';
 
 class Sidebar extends React.Component<any, any> {
+    public linkClass(url: string) {
+        const { pathname } = this.props.router.location;
+        return `sidebar-icon-wrapper ${pathname === url ? 'active' : 'null'}`;
+    }
 
     public render() {
         return (
             <div className='sidebar-wrapper'>
                 <div className='sidebar-list'>
-                    <a className='sidebar-icon-wrapper' onClick={() => this.props.navigate('/')} >
+                    <a className={this.linkClass('/')} onClick={() => this.props.navigate('/')} >
                         <span className='material-icons sidebar-icon'>poll</span>
                     </a>
-                    <a className='sidebar-icon-wrapper'>
+                    <a className='sidebar-icon-wrapper disabled'>
                         <span className='material-icons sidebar-icon'>explore</span>
                     </a>
-                    <a className='sidebar-icon-wrapper'>
-                        <span className='material-icons sidebar-icon'>favorite_border</span>
-                    </a>
-                    <a className='sidebar-icon-wrapper'>
-                        <span className='material-icons sidebar-icon'>watch_later</span>
-                    </a>
+                    {
+                        this.props.user.loggedIn &&
+                        <div className='sidebar-list__restricted'>
+                            <a className='sidebar-icon-wrapper'>
+                                <span className='material-icons sidebar-icon'>favorite_border</span>
+                            </a>
+                            <a className='sidebar-icon-wrapper'>
+                                <span className='material-icons sidebar-icon'>watch_later</span>
+                            </a>
+                        </div>
+                    }
                 </div>
             </div>
         );
@@ -31,7 +41,7 @@ const mapState = (state: any) => {
     return { user, router };
 };
 
-const mapDispatch = (dispatch: any) => ({
+const mapDispatch = (dispatch: Dispatch<any>) => ({
     navigate: (url: string) => dispatch(push(url))
 });
 
